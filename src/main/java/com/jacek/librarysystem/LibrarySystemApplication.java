2f07d6a984111e5e49096f1067396edc2ce4c244
jacekgry <jacek.grycza@student.put.poznan.pl>
2019-05-22 17:00:49 +0200

    H2

--- src/main/java/com/jacek/librarysystem/LibrarySystemApplication.java
+++ src/main/java/com/jacek/librarysystem/LibrarySystemApplication.java
@@ -10,4 +10,5 @@ public class LibrarySystemApplication {
         SpringApplication.run(LibrarySystemApplication.class, args);
     }
 
+    
 }
---
+++ src/main/java/com/jacek/librarysystem/security/SecurityConfiguration.java
@@ -0,0 +1,24 @@
+package com.jacek.librarysystem.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/h2-console/**").permitAll()
+                .anyRequest().authenticated().and().httpBasic();
+        http.headers().frameOptions().disable();
+    }
+}
